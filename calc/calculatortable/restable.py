cea23c55c82e0fff2f0c4d52f4198dca09d3aab9
KendallEverly <77425679+KendallEverly@users.noreply.github.com>
2021-12-17 00:25:28 -0800

    updated previous to project 3

---
+++ calc/calculatortable/restable.py
@@ -0,0 +1,32 @@
+# f = open('calcresults.csv')
+#
+# def write_file(data):
+#     with open('datacalc.txt', 'a') as f:
+#         f.append(data + '\n')
+
+# class User(db.Model):
+#    value1 = db.Column(db.Integer, index=True)
+#    value2 = db.Column(db.Integer, index=True)
+#    operation = db.Column(db.String(30))
+#    result = db.Column(db.Integer, index=True)
+#
+#  def to_dict(self):
+#         return {
+#             'value1': self.value1,
+#             'value2': self.value2,
+#             'operation': self.operation,
+#             'result': self.result,
+#         }
+# import pandas as pd
+#
+# # to read csv file named "samplee"
+# a = pd.read_csv("C:\\Users\\kenda\\PycharmProjects\\Validationhw\\validationnhw\\csv\\calcresults.csv")
+# df.to_csv('calcresults.csv', index=None)
+
+# # to save as html file
+# # named as "Table"
+# a.to_html("calctable.html")
+#
+# # assign it to a
+# # variable (string)
+# html_file = a.to_html()
